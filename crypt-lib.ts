0b68c4788142d6cf01655132bffbdf439d9dd530
John Waweru <wambugu.john.waweru@outlook.com>
2017-03-03 11:14:29 +0300

    added type and fixed generic function return type warning

--- crypt-lib.ts
+++ crypt-lib.ts
@@ -8,33 +8,40 @@ import * as assert from 'assert';
 export namespace jwCrypt{
 
     export function encrypt<T>(data: T, algorithm: string, key: string): string {
-        let data_string: string = JSON.stringify(data); 
-        let cipher = crypto.createCipher(algorithm, key);
-        let digest = cipher.update(data_string, 'utf8', 'hex');
+        let data_string: string = JSON.stringify(data); //#note: create a string out of the data structure provided
+        let cipher: crypto.Cipher = crypto.createCipher(algorithm, key);
+        let digest: string = cipher.update(data_string, 'utf8', 'hex');
         digest += cipher.final('hex');
         return digest;
     }
 
-    export function decrypt<T>(data: string, algorithm: string, key: string): T | string {
-        let decipher = crypto.createDecipher(algorithm, key);
-        let digest = decipher.update(data, 'hex', 'utf8');
+    export function decrypt<T>(data: string, algorithm: string, key: string): T {
+        let decipher: crypto.Decipher = crypto.createDecipher(algorithm, key);
+        let digest: string = decipher.update(data, 'hex', 'utf8');
         digest += decipher.final('utf8');
-        return JSON.parse(digest);
+        return JSON.parse(digest); //#note: return the data structure that was encrypted in the first place
     }
 }
 
 //#Ingore: this is a simple test for the above code.
-let password: string = `metoyourpassword`;
+interface credentials {
+    hash_password: string;
+    auth_token: string;
+}
+let data: credentials = {
+    hash_password: `ahashedpasswordisthebest`,
+    auth_token: `thisismytoken123`
+};
 let key: string = `ineedmoney`;
 let algorithm: string = `aes-256-ctr`;
-let encryptedPassword: string = jwCrypt.encrypt<string>(password, algorithm, key);
-let decryptedPassword: string = jwCrypt.decrypt<string>(encryptedPassword, algorithm, key);
+let encrypted_data: string = jwCrypt.encrypt<credentials>(data, algorithm, key);
+let decrypted_data: credentials = jwCrypt.decrypt<credentials>(encrypted_data, algorithm, key);
 //@test: make sure decrypted password is the same as the original
-assert.deepStrictEqual({a:password}, {a:decryptedPassword});
+assert.deepStrictEqual({ a: data }, { a: decrypted_data});
 //@log: display process
 console.log(`
-    Original data: ${password}\n
-    Encrypted data: ${encryptedPassword}\n 
-    Decrypted data: ${decryptedPassword}\n 
+    Original data:- password => ${decrypted_data.hash_password} and auth token => ${decrypted_data.auth_token}\n
+    Encrypted data:- ${encrypted_data}\n 
+    Decrypted data:- password => ${decrypted_data.hash_password} and auth token => ${decrypted_data.auth_token}\n 
     Completed...
 `);
