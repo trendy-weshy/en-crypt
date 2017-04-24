96221e7e1e9604f7375e98139592d745dc8ba452
John Waweru <wambugu.john.waweru@outlook.com>
2017-04-24 11:33:17 +0300

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * created by waweru
+ */
+
+'use strict';
+
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('compile', () => {
+  return tsProject
+    .src()
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest('./'));
+});
