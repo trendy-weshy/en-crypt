19d928bc69b297ac77465c5ab563d9fb7bb58342
John Waweru <wambugu.john.waweru@outlook.com>
2017-04-24 12:49:06 +0300

    first test suite implemented

--- test/index.ts
+++ test/index.ts
@@ -4,6 +4,33 @@
  * LICENSE: MIT
  */
 
-(function () {
-  console.log(`Hello, tests`);
-})();
+import * as assert from 'assert';
+import {TwCrypt} from '../index';
+
+interface ICredentials {
+    hash_password: string;
+    auth_token: string;
+}
+
+
+describe('TwCrypt Test Suite', function() {
+  
+  it('Should encrypt and decrypt an object', (done: any) => {
+
+    let data: ICredentials = {
+      hash_password: `ahashedpasswordisthebest`,
+      auth_token: `thisismytoken123`
+    };
+    
+    let key: string = `ineedmoney`;
+    let algorithm: string = `aes-256-ctr`;
+
+    let encrypted_data: string = TwCrypt.encrypt<ICredentials>(data, algorithm, key);
+    let decrypted_data: ICredentials = TwCrypt.decrypt<ICredentials>(encrypted_data, algorithm, key);
+
+    assert.deepStrictEqual({ a: data }, { a: decrypted_data});
+    return done();
+
+  });
+
+});
