7f963da7bc5e89270cd499384260b504de09a6c7
John Waweru <wambugu.john.waweru@outlook.com>
2017-06-01 07:26:34 +0300

    added list of supported ciphers

--- index.ts
+++ index.ts
@@ -3,26 +3,12 @@
  * created by waweru
  */
 
-export default class Encrypt<T> {
+const cipherList = require('./en_crypt.json');
+
+export class SlowdayEncrypt {
+
+    public static decrypt<T>() {}
+
+    public static encrypt<T>() {}
     
-    public static inst;
-
-    public static init<G>() {
-        if (!Encrypt.inst) {
-          Encrypt.inst = new Encrypt<G>();
-        }
-        return Encrypt.inst;
-    }
-
-    private constructor() {
-        // generate an algorithm to be used by the singleton
-    }
-
-    private cipher(data: T, algorithm: string, key: string): string {
-        return '';
-    }
-
-    private decipher(data: string, algorithm: string, key: string): T {
-        return {} as T;
-    }
 }
