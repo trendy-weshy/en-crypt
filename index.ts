3339f558bd417aecee20f53f3f6776b14e3c3207
John Waweru <wambugu.john.waweru@outlook.com>
2017-06-01 07:35:19 +0300

    fixed linting issues

--- index.ts
+++ index.ts
@@ -5,12 +5,17 @@
 
 import {randomBytes, createCipher, Cipher, createDecipher, Decipher} from 'crypto';
 
+// tslint:disable-next-line:no-var-requires
 const cipherList = require('./en_crypt.json');
 
 export class SlowdayCrypt {
 
-    public static decrypt<T>() {}
+    public static decrypt<T>() {
+        //
+    }
 
-    public static encrypt<T>() {}
-    
+    public static encrypt<T>() {
+        //
+    }
+
 }
